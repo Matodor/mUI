a3d03c36d19ea13c32a41745fe8dcc7b4e0ac0d9
Matodor <matodor-zlo@yandex.ru>
2018-06-23 19:28:24 +0500

    InternetConnectionChecker PingAsync

--- mFramework/src/concrete/InternetConnectionChecker.cs
+++ mFramework/src/concrete/InternetConnectionChecker.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.NetworkInformation;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,6 +10,40 @@ namespace mFramework.concrete
 {
     public class InternetConnectionChecker
     {
+        public static void PingAsync(Action onSuccess, Action<Exception> onFailed, int timeout = 10000)
+        {
+            try
+            {
+                var ping = new Ping();
+                var host = "8.8.8.8";
+                var buffer = new byte[32];
+                var pingOptions = new PingOptions();
+                var result = ping.SendPingAsync(host, timeout, buffer, pingOptions);
+
+                result.ContinueWith(reply =>
+                {
+                    if (result.Result.Status == IPStatus.Success)
+                        onSuccess?.Invoke();
+                    else
+                        onFailed?.Invoke(result.Exception);
+                }, TaskContinuationOptions.OnlyOnRanToCompletion);
+
+                result.ContinueWith(reply =>
+                {
+                    onFailed?.Invoke(reply.Exception);
+                }, TaskContinuationOptions.OnlyOnFaulted);
+
+                result.ContinueWith(reply =>
+                {
+                    onFailed?.Invoke(reply.Exception);
+                }, TaskContinuationOptions.OnlyOnCanceled);
+            }
+            catch (Exception exception)
+            {
+                onFailed?.Invoke(exception);
+            }
+        }
+
         public static void CheckAsync(Action onSuccess, Action<Exception> onFailed)
         {
             var result = GetPageAsync(new Uri("http://google.com"));
@@ -25,12 +60,19 @@ namespace mFramework.concrete
             {
                 onFailed?.Invoke(task.Exception);
             }, TaskContinuationOptions.OnlyOnFaulted);
+
+            result.ContinueWith(task =>
+            {
+                onFailed?.Invoke(task.Exception);
+            }, TaskContinuationOptions.OnlyOnCanceled);
         }
 
         private static async Task<bool> GetPageAsync(Uri uri)
         {
             using (var client = new HttpClient())
             {
+                client.Timeout = TimeSpan.FromSeconds(10);
+                
                 using (var response = await client.GetAsync(uri))
                 {
                     var success = (int) response.StatusCode >= 200 && (int) response.StatusCode < 299;
